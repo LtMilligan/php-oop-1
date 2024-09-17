d46917e58b8abc5a8d659090cf53e6603df8b207
LtMilligan <gioele.miscia@gmail.com>
2024-09-17 15:37:06 +0200

    [UPD] Adding variables and construct

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
+class Movie {
 
+    public $titolo;
+    public $anno;
+    public $genere;
+    public $durata;
+    public $descrizione;
 
+    public function __construct($_titolo,  $_anno, $_genere, $_durata, $_descrizione) {
+
+        $this->titolo =  $_titolo;
+        $this->anno =  $_anno;
+        $this->genere = $_genere;
+        $this->durata = $_durata;
+        $this->descrizione = $_descrizione;
+
+    }
+}
 ?>
